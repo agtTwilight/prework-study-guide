e52f54d5cf10c2a94532198168cf6b435efb1ba2
agtTwilight <lukasmacmillen@gmail.com>
2022-12-09 12:37:40 -0800

    Added functions to the JS code

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,25 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(var i=0; i<topics.length; i++){
-    console.log(topics[i])
+function listTopics(){
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic(){
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
 
 /*
 var shapes = ["triangle", "square", "pentagon", "circle"];
@@ -25,3 +28,8 @@ for(var i = 0; i<shapes.length; i++){
     //Explain the logic within the for loop. Moving i++ to the position of this comment makes more sense w/ control flow.
 }
 */
+
+console.log("The following is a list of topics that we learned through the Prework")
+listTopics()
+console.log("Which topic should we study first?")
+selectTopic()
